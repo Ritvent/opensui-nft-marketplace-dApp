7c500dbe92dd6f6bf79c872759a481c2b8ab4430
Ritvent <ivanventurilloq@gmail.com>
2025-11-03 12:08:29 +0800

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { createNetworkConfig, SuiClientProvider, WalletProvider, ConnectButton } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Minter from './components/Mint';
+import './App.css';
+import { StrictMode } from 'react';
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl('localnet') },
+  testnet: { url: getFullnodeUrl('testnet') },
+  mainnet: { url: getFullnodeUrl('mainnet') },
+});
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <SuiClientProvider networks={networkConfig} defaultNetwork='testnet'>
+                <WalletProvider>
+                  <header className='app-header'>
+                    <ConnectButton />
+                  </header>
+                  <main className='app-main'>
+                    <Minter />
+                  </main>
+                </WalletProvider>
+            </SuiClientProvider>
+        </QueryClientProvider>
+      </StrictMode>
+  );
+}
+
+export default App
---
+++ src/configs/constants.ts
@@ -0,0 +1,3 @@
+export const CONTRACTPACKAGEID = "0x6a8a82df94142a6889b26f44069c72a7b85db48d0b4545657206e03653ceffe3";
+export const CONTRACTMODULENAME = "nft_marketplace";
+export const CONTRACTMODULEMETHOD = "mint_to_sender";
